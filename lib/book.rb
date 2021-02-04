f55bdccdb6a888a2862fe2e2597076953ea0b7a2
Keginold Philip <keginoldp@hotmail.com>
2021-02-04 16:34:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,25 +1,27 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
-	class Book
-		def initialize(title)
-			@title = title
-		end
+  def title
+    @title
+  end
 
-		def title
-			@title
-		end
+  def author=(author)
+    @author = author
+  end
 
-		def author=(author)
-			@author = author
-		end
+  def author
+    @author
+  end
 
-		def page_count=(num)
-	 @page_count = num
- end
+  def page_count=(num)
+    @page_count = num
+  end
 
- def page_count
-	 @page_count
- end
-
+  def page_count
+    @page_count
+  end
 
   def genre=(genre)
     @genre = genre
@@ -29,7 +31,8 @@
     @genre
   end
 
-	def turn_page
-	 puts "Flipping the page...wow, you read fast!"
- end
-	end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
